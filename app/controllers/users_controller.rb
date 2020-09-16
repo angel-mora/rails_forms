75f43b63266308263eff2a5537883e5f509bf590
AngelMoraDev <angelmomaa@gmail.com>
2020-09-16 17:07:39 -0500

    working in html forms section

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,23 +5,22 @@ class UsersController < ApplicationController
     end
 
     def create
-       # below code intends to create a new record in the users table
-       # @user is an instance variable, 
-       # User refers to the model because it inherites from <UsersController>
-       # .new is the methord that will build the object making ready to be added as a new record
-       # .save saves the new record to th users database
-        
+        # @user = User.new(user_params)
 
-        #create- User.create
-
-        if User.create(username: params[:username], email: params[:email], password: params[:password])
+        if @user.save
             redirect_to new_user_path
-        else 
-            render :new 
+        else
+            render :new
         end
-
-
-          
     end
-    
+
+    # private
+    def user_params
+        @user = {
+            username: params[:username],
+            email: params[:email],
+            password: params[:password] }
+        params.require(:user).permit(:username, :email, :password)
+    end
+
 end
