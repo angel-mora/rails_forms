ff95c89edcaa0d0fbe282dcd02d183644c007274
gabrielHilarion <gabrielhhilarion@gmail.com>
2020-09-16 23:41:59 +0100

    add new user using private method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,29 +5,17 @@ class UsersController < ApplicationController
     end
 
     def create
-        # @user = User.new(user_params)
+        @user = User.new(user_params)
 
-<<<<<<< HEAD
-         if User.create(username: params[:username], email: params[:email], password: params[:password])
-             redirect_to new_user_path
-         else 
-             render :new 
-         end
-=======
         if @user.save
             redirect_to new_user_path
         else
             render :new
         end
     end
->>>>>>> 75f43b63266308263eff2a5537883e5f509bf590
 
-    # private
+    private
     def user_params
-        @user = {
-            username: params[:username],
-            email: params[:email],
-            password: params[:password] }
         params.require(:user).permit(:username, :email, :password)
     end
 
