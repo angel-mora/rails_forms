b46449eda2ca30a5b160bc8555bd452de6ff2396
gabrielHilarion <gabrielhhilarion@gmail.com>
2020-09-22 16:27:51 +0100

    create edit view, update routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,19 @@
 class UsersController < ApplicationController
     
+    def index
+        @user = User.all
+    end
+
     def new
-        #@user = User.all
+        @user = User.new
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def show
+        @user = User.find(params[:id])
     end
 
     def create
@@ -11,6 +23,16 @@ class UsersController < ApplicationController
             redirect_to new_user_path
         else
             render :new
+        end
+    end
+
+    def update
+        @user = User.find(params[:id])
+       
+        if @user.update(user_params)
+          redirect_to @user
+        else
+          render 'edit'
         end
     end
 
